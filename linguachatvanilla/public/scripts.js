f91040d0c0c332ce1ff7a04918bfdcf01c68fef2
Esty <esty5280@gmail.com>
2023-02-15 13:43:09 +0200

    settings page

--- linguachatvanilla/public/scripts.js
+++ linguachatvanilla/public/scripts.js
@@ -1,4 +1,19 @@
 
+
+
+//home
+let languageBeingLearned = "English";
+let get = document.getElementsByClassName('lang-item')
+for(let i=0;i<get.length;i++){
+
+
+get[i].addEventListener('click', () => {
+    languageBeingLearned = get[i].innerText;
+    console.log("You will now be learning " + languageBeingLearned)
+})
+}
+
+//posts
 function showHideComments(){
     console.log("entered function")
     let d = document.getElementById("comments");
@@ -8,44 +23,28 @@ function showHideComments(){
     d.style.display="none"
 }
 
-
-// let icon=document.querySelector('.hamburger-icon')
-// icon.addEventListener('click', () => {
-//     document.querySelector('.nav-menu').style.display = "block"
-// })
-
-
+//memory card game
 let englishWords=["where","there","dog","table","door","onion","hands","chair","teacher","suitcase","finger","tree"]
 let hebrewWords=["איפה","שם","כלב","שולחן","דלת","בצל","ידיים","כסא","מורה","מזוודה","אצבע","עץ"]
 let allWords=englishWords.concat(hebrewWords)
-
 let randomNumberArray=[]
 
-
-
-for(let i=0;i<allWords.length;i++){
-
-    let randomNumber=Math.floor(Math.random() * 24)
+for(let i=0;i<allWords.length;i++) {
+    let randomNumber=Math.floor(Math.random() * allWords.length)
     while (randomNumberArray.indexOf(randomNumber) != -1)
-    randomNumber=Math.floor(Math.random() * 24)
-    console.log(randomNumber)
+    randomNumber=Math.floor(Math.random() * allWords.length)
     randomNumberArray.push(randomNumber)
-let memoryCard=document.createElement('div')
-memoryCard.classList.add('one-card')
-let backFace=document.createElement('div')
-let frontFace = document.createElement('div')
-// frontFace.classList.add('one-card')
-frontFace.classList.add('front-face')
-frontFace.innerText = allWords[randomNumber] //???????
-// backFace.classList.add('one-card')
-backFace.classList.add('back-face')
-// backFace.style.backgroundImage = "url('http:///192.168.30.157:8080/backImage.png')"
-backFace.style.backgroundImage = "url('./img/backImage.png')"
-
-
-memoryCard.appendChild(backFace)
-memoryCard.appendChild(frontFace)
-document.querySelector('.all-cards').appendChild(memoryCard)
+    let memoryCard = document.createElement('div')
+    memoryCard.classList.add('one-card')
+    let backFace=document.createElement('div')
+    let frontFace = document.createElement('div')
+    frontFace.classList.add('front-face')
+    frontFace.innerText = allWords[randomNumber] //???????
+    backFace.classList.add('back-face')
+    backFace.style.backgroundImage = "url('./img/backImage.png')"
+    memoryCard.appendChild(backFace)
+    memoryCard.appendChild(frontFace)
+    document.querySelector('.all-cards').appendChild(memoryCard)
 }
 
 let firstCard, secondcard;
@@ -57,15 +56,12 @@ function flipCard() {
     if (lockBoard) return;
     if(this === firstCard) return;
     this.classList.add('flip')
-
     if(!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
         return;
     }
     secondcard = this;
-    // hasFlippedCard=false;
-
     checkForMatch();
 }
 
@@ -110,12 +106,81 @@ function resetBoard(){
     [firstCard, secondcard] = [null, null];
 }
 
-// function shuffle() {
-//     +   cards.forEach(card => {
-//     +     let randomPos = Math.floor(Math.random() * 12);
-//     +     card.style.order = ramdomPos;
-//     +   });
-//     + })();
-
 cards.forEach(card => card.addEventListener('click', flipCard))
 
+//hangman
+function hangman() {
+let englishAlphabet =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+//  console.log(englishAlphabet)
+let russianAlphabet = [];
+let hebrewAlphabet= [];
+let spanishAlphabet = [];
+let frenchAlphabet = [];
+// console.log(0063)
+if(languageBeingLearned == "English"){
+    console.log(englishAlphabet)
+    for(let j=0; j<englishAlphabet.length; j++){
+        let d = document.createElement('div')
+        d.innerText = englishAlphabet[j];
+        document.querySelector(".alphabet").appendChild(d)
+    }
+}
+
+}
+
+//settings
+function showSelectedContent() {
+    
+let content=document.querySelectorAll('.content')
+for(let m=1; m<content.length;m++){
+content[m].style.display="none";
+}
+
+
+let sidenav = document.querySelectorAll('.sidenav li');
+for(let x=0;x<sidenav.length;x++){
+    sidenav[x].addEventListener('click',()=>{
+        // forEach(sidenav) =>{}
+        sidenav.forEach((g)=>{
+            g.style.backgroundColor="rgb(18, 43, 29)"
+            g.style.color="rgba(194, 177, 177, 0.753)"
+        })
+        sidenav[x].style.color = "rgb(18, 43, 29)"
+        sidenav[x].style.backgroundColor= "rgba(194, 177, 177, 0.753)"
+        let content=document.querySelectorAll('.content')
+for(let m=0; m<content.length;m++){
+content[m].style.display="none";
+}
+content[x].style.display="block";
+
+ })
+
+}
+//let selection = 
+
+// switch (selection) {
+//     case "general":
+//       day = "Sunday";
+//       break;
+//     case 1:
+//       day = "Monday";
+//       break;
+//     case 2:
+//        day = "Tuesday";
+//       break;
+//     case 3:
+//       day = "Wednesday";
+//       break;
+//     case 4:
+//       day = "Thursday";
+//       break;
+//     case 5:
+//       day = "Friday";
+//       break;
+//     case 6:
+//       day = "Saturday";
+//   }
+
+}
--- linguachatvanilla/src/app.js
+++ linguachatvanilla/src/app.js
@@ -1,11 +1,56 @@
 const express = require("express");
 const path = require("path")
 const fs = require('fs')
+const http = require('http')
+// const mysql = require('mysql');
+// const session = require('express-session')
+const {I18n} = require('i18n')
+const i18n = new I18n({
+    locales: ['en', 'he'],
+    directory: path.join(__dirname, 'locales')
+})
+
+const my18 = http.createServer((req, res) =>{
+    i18n.init(req, res)
+    res.end(res.__('Hello'))
+})
+
+
+// const connection  = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'nodelogin'
+// });
+
+// connection.connect(function(err) {
+//     if(err){
+//         return console.error('error: '+err.message);
+//     }
+//     console.log('connected to the MYSQL server.')
+// })
 
 const app = express()
 
+// app.use(session({
+//     secret: 'secret',
+//     resave: true,
+//     saveUninitialized: true
+// }))
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}))
+
 //set static folder
 app.use('/public', express.static(path.join(__dirname, '../public')))
+
+
+app.get('/login', function(request, response){
+    response.sendFile(path.join(__dirname+'/login.html'))
+})
+
+
+
 
 app.get('/', (req, res)=>{
     res.send('hello from express')
